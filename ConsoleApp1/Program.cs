dd7b2a8e96530eb226f6d14c9efc632b178267f4
Marco M <marco.m12@hotmail.de>
2019-08-08 16:33:02 +0200

    Added material calculator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RecipeCalculator
 {
@@ -26,9 +22,9 @@ namespace RecipeCalculator
             double goldenDaric_Price = Convert.ToDouble(Console.ReadLine());
 
             //Debug outputs.
-                //Console.WriteLine(goldenTalent_Price.ToString());
-                //Console.WriteLine(productionPoint_Price.ToString());
-                //Console.WriteLine(goldenDaric_Price.ToString());
+            //Console.WriteLine(goldenTalent_Price.ToString());
+            //Console.WriteLine(productionPoint_Price.ToString());
+            //Console.WriteLine(goldenDaric_Price.ToString());
 
             //Calculates the gold value of one single production point if bought from the trade broker.
             double singlePP_Price = productionPoint_Price / 4000;
@@ -49,10 +45,103 @@ namespace RecipeCalculator
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("You gain " + profit + " gold per produce.");
             }
-            else {
+            else
+            {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("You lose " + profit * -1 + " gold per produce.");
+            }
+
+            //Material calculator. You enter your materials for this recipe and get told which ones you need to stock up on and how many.
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("If you would like to proceed with the material calculator type 'Yes' and hit enter. ");
+            string answer = Console.ReadLine();
+            if (answer == "Yes")
+            {
+
+                Console.Write("How many Golden Talents do you have? ");
+                double materialA = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("How many Craft Kits do you have? ");
+                double materialB = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("How many Production points do you have? ");
+                double materialC = Convert.ToDouble(Console.ReadLine());
+
+                /* My goal here is to find out which material the player has most of relative to the recipe ratios.
+                 * 
+                 * The ratios are as followed:
+                 * Material A (Golden Talent) 5
+                 * Material B (Craft Kits) 60
+                 * Material C (Production Points) 20
+                 * 
+                 * Following that, I divide each number by the number needed for to craft one produce (5, 60 and 20).
+                 * Whichever value ends up being the highest is now the goal for the other materials to be stocked up to.
+                 * For example: Player has enough mats C to craft 60 produce. Mats A and B will subsequently be stocked up to get to 60.
+                 */
+
+                //Aux is short for auxiliary calculation don't sue me
+                double auxA = materialA / 5;
+                double auxB = materialB / 60;
+                double auxC = materialC / 20;
+
+                //Rounding down to assure even numbers and that the highest number of mats does not need increasing. 
+                //This might cause you to have leftovers of the material that you have most of.
+                //Yes, this is messy but sue me.
+                int auxARounded = Convert.ToInt32(auxA);
+                int auxBRounded = Convert.ToInt32(auxB);
+                int auxCRounded = Convert.ToInt32(auxC);
+
+                //This next part is an overcomplicated way of finding out which rounded number is the highest
+
+                if (auxARounded > auxBRounded & auxARounded > auxCRounded)
+                {
+                    //Calculating the need of material B; Max amount of produce according to material A being the highest quantitative standard
+                    //minus the material B's you already have = the material B's you still need.
+                    double materialBNeed = Convert.ToDouble(auxARounded) * 60 - materialB;
+                    double materialCNeed = Convert.ToDouble(auxARounded) * 20 - materialC;
+                    Console.WriteLine("You need to buy a total of " + materialBNeed + " Craft Kits and " + materialCNeed + " production points.");
                 }
+                else if (auxBRounded > auxARounded & auxBRounded > auxCRounded)
+                {
+                    double materialANeed = Convert.ToDouble(auxBRounded) * 5 - materialA;
+                    double materialCNeed = Convert.ToDouble(auxBRounded) * 20 - materialC;
+                    Console.WriteLine("You need to buy a total of " + materialANeed + " Golden Talents and " + materialCNeed + " production points.");
+                }
+                else if (auxCRounded > auxARounded & auxCRounded > auxBRounded)
+                {
+                    double materialANeed = Convert.ToDouble(auxCRounded) * 5 - materialA;
+                    double materialBNeed = Convert.ToDouble(auxCRounded) * 60 - materialB;
+                    Console.WriteLine("You need to buy a total of " + materialANeed + " Golden Talents and " + materialBNeed + " Craft Kits.");
+                }
+                //---*--- RARE-ish EXCEPTIONS ---*---
+                //All of them can produce the same amount of produce or there is no produce that can be crafted
+                else if (auxARounded.Equals(auxBRounded & auxCRounded))
+                {
+                    Console.WriteLine("You either already have the perfect amount of materials or not enough of all of them. Stock up on one of them and retry!");
+                }
+                //A equals B and both are bigger than C
+                else if (auxARounded.Equals(auxBRounded) & auxARounded > auxCRounded)
+                {
+                    double materialCNeed = Convert.ToDouble(auxARounded) * 20 - materialC;
+                    Console.WriteLine("You are missing " + materialCNeed + " production points. Consider buying an Elinu's Tear potion from the Trade Broker or Vanguard Special Rewards Vendor.");
+                }
+                //A equals C and both are bigger than B
+                else if (auxARounded.Equals(auxCRounded) & auxARounded > auxBRounded)
+                {
+                    double materialBNeed = Convert.ToDouble(auxARounded) * 60 - materialB;
+                    Console.WriteLine("You are missing " + materialBNeed + " Craft Kits.");
+                }
+                //B equals C and both are bigger than A
+                else if (auxBRounded.Equals(auxCRounded) & auxBRounded > auxARounded)
+                {
+                    double materialANeed = Convert.ToDouble(auxARounded) * 5 - materialA;
+                    Console.WriteLine("You are missing " + materialANeed + " Golden Talents.");
+                }
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
